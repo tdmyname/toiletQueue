9838fc7913e50a4c3467551847731615cc81373b
tanDong <18904500435@163.com>
2017-01-19 10:47:19 +0800

    当cookie有值则设为首次加载时的默认值

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,13 @@ import {combineReducers} from "redux"
 import {createReducer} from "redux-act"
 import {userLogin, subscribe} from "../actions"
 import {setCookies, getCookies} from "../containers/myCookies"
+const initialUser = getCookies(userLogin.getType()) != "" && getCookies(userLogin.getType()) != undefined && getCookies(userLogin.getType()) != null ? getCookies(userLogin.getType()) : ""
 const login = createReducer({
     [userLogin]: (state, user) => {
         setCookies(userLogin.getType(), user, 1)
         return user
     }
-}, getCookies(userLogin.getType()) != "" && getCookies(userLogin.getType()) != undefined && getCookies(userLogin.getType()) != null ? getCookies(userLogin.getType()) : "")
+}, initialUser)
 const userList = createReducer({
     [subscribe]: (state, users) => {
         return [...users]
